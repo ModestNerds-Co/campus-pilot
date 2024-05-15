ef365bb2f9750233a87a23537130ddcf6182ae8d
Ngonidzashe Mangudya <imngonii@gmail.com>
2024-05-15 23:00:56 +0200

    chore: setup project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.campus_pilot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:campus_pilot/app/app.dart';
+import 'package:campus_pilot/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
