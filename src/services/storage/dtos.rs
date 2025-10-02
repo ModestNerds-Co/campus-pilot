6a9c20a819d5f07abcd0ef0bcc0771dd0f20ec02
Ngonidzashe Mangudya <imngonii@gmail.com>
2025-10-02 21:52:17 +0200

    feat: ✨ Add UploadHeaders to GenerateUploadUrlResponse

--- src/services/storage/dtos.rs
+++ src/services/storage/dtos.rs
@@ -22,4 +22,11 @@ pub struct GenerateUploadUrlResponse {
     pub upload_url: String,
     pub file_key: String,
     pub expires_in: u64,
+    pub headers: UploadHeaders,
+}
+
+#[derive(Serialize)]
+pub struct UploadHeaders {
+    #[serde(rename = "x-amz-acl")]
+    pub acl: String,
 }
--- src/services/storage/routes.rs
+++ src/services/storage/routes.rs
@@ -12,7 +12,7 @@ use validator::Validate;
 
 use crate::{
     models::api_response::ApiResponse,
-    services::storage::dtos::{GenerateUploadUrlRequest, GenerateUploadUrlResponse},
+    services::storage::dtos::{GenerateUploadUrlRequest, GenerateUploadUrlResponse, UploadHeaders},
     state::AppState,
     utils::flatten_validation_errors,
 };
@@ -52,6 +52,9 @@ pub async fn generate_upload_url(
                 upload_url,
                 file_key,
                 expires_in,
+                headers: UploadHeaders {
+                    acl: "public-read".to_string(),
+                },
             };
 
             Ok(HttpResponse::Ok().json(ApiResponse::from_status(
